b9fe9f7218cecbed7ed2339a1fc82442b1a1a3e9
Slow <7slow7poison7@gmail.com>
2024-07-07 11:37:24 +0200

    FIX: enhance responivens

--- update_user.php
+++ update_user.php
@@ -71,37 +71,31 @@ if (isset($_POST['submit'])) {
    <?php include 'components/user_header.php'; ?>
 
    <section class="form-container">
+      <div class="form-container-inner">
+         <div class="container">
+            <div class="row  justify-content-center">
+               <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-5">
+                  <form action="" method="post">
+                     <h3>Update now</h3>
+                     <input type="hidden" name="prev_pass" value="<?= $fetch_profile["password"]; ?>">
+                     <input type="text" name="name" required placeholder="enter your username" maxlength="20"
+                        class="box" value="<?= $fetch_profile["name"]; ?>">
+                     <input type="email" name="email" required placeholder="enter your email" maxlength="50" class="box"
+                        oninput="this.value = this.value.replace(/\s/g, '')" value="<?= $fetch_profile["email"]; ?>">
+                     <input type="password" name="old_pass" placeholder="enter your old password" maxlength="20"
+                        class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+                     <input type="password" name="new_pass" placeholder="enter your new password" maxlength="20"
+                        class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+                     <input type="password" name="cpass" placeholder="confirm your new password" maxlength="20"
+                        class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+                     <input type="submit" value="update now" class="btn" name="submit">
+                  </form>
+               </div>
+            </div>
 
-      <form action="" method="post">
-         <h3>Update now</h3>
-         <input type="hidden" name="prev_pass" value="<?= $fetch_profile["password"]; ?>">
-         <input type="text" name="name" required placeholder="enter your username" maxlength="20" class="box"
-            value="<?= $fetch_profile["name"]; ?>">
-         <input type="email" name="email" required placeholder="enter your email" maxlength="50" class="box"
-            oninput="this.value = this.value.replace(/\s/g, '')" value="<?= $fetch_profile["email"]; ?>">
-         <input type="password" name="old_pass" placeholder="enter your old password" maxlength="20" class="box"
-            oninput="this.value = this.value.replace(/\s/g, '')">
-         <input type="password" name="new_pass" placeholder="enter your new password" maxlength="20" class="box"
-            oninput="this.value = this.value.replace(/\s/g, '')">
-         <input type="password" name="cpass" placeholder="confirm your new password" maxlength="20" class="box"
-            oninput="this.value = this.value.replace(/\s/g, '')">
-         <input type="submit" value="update now" class="btn" name="submit">
-      </form>
-
+         </div>
+      </div>
    </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
    <?php include 'components/footer.php'; ?>
 
    <script src="js/script.js"></script>
